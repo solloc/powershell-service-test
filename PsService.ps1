648b0a0337cd42281112671598185ef0c34b7e4e
solloc <matthias@maisch.me>
2022-05-05 21:44:00 +0200

    blog referenced

--- PsService.ps1
+++ PsService.ps1
@@ -1,3 +1,6 @@
+# Original Script https://github.com/JFLarvoire/SysToolsLib/blob/master/PowerShell/PSService.ps1
+# Blog: https://docs.microsoft.com/en-us/archive/msdn-magazine/2016/may/windows-powershell-writing-windows-services-in-powershell
+
 Write-Output "Hello world"
 
 $source = @"
