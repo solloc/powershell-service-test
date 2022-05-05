d83fa883c851ad0cb9b966a75f8378b12d9f3db4
solloc <matthias@maisch.me>
2022-05-05 21:25:52 +0200

    first c# class included

--- PsService.ps1
+++ PsService.ps1
@@ -1,1 +1,16 @@
 Write-Output "Hello world"
+
+$source = @"
+    using System;
+    class BasicTest
+    {
+        static void Main() {
+            Console.WriteLine("Hello From C#");
+        }
+
+    }
+"@
+
+Write-Output $basicTest.ToString()
+
+Add-Type -TypeDefinition $source -OutputAssembly "service.exe"
